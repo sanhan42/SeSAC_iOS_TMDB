701cbac3e88b316b5a0e263edf8c0d998051c0d8
sanhan42 <sangmin613@gmail.com>
2022-08-06 19:53:28 +0900

    Make APIManager

---
+++ TMDB/APIManager/TrendMediaAPIManager.swift
@@ -0,0 +1,85 @@
+//
+//  TrendMediaAPIManager.swift
+//  TMDB
+//
+//  Created by 한상민 on 2022/08/06.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class TrendMediaAPIManager {
+    let semaphore = DispatchSemaphore(value: 1)
+    
+    static let shared = TrendMediaAPIManager()
+    private init () {}
+    
+    typealias CompletionHandler = (Int, [Movie]) -> ()
+    
+    func getGenresData() -> [Int:String] {
+        var genres: [Int:String] = [:]
+        let url = EndPoint.genreURL + APIKey.TMDB + Language.korean
+        semaphore.wait()
+        AF.request(url, method: .get).validate(statusCode: 200...300).responseData(queue: .global()) { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                let dicValue = json["genres"].arrayValue.map {($0["id"].intValue, $0["name"].stringValue)}
+                genres = Dictionary(uniqueKeysWithValues: dicValue)
+//                print(genres)
+            case .failure(let error):
+                print(error)
+            }
+            self.semaphore.signal()
+        }
+        semaphore.wait()
+        semaphore.signal()
+        return genres
+    }
+    
+    func getCast(id: Int) -> [Cast] {
+        let creditsUrl = EndPoint.movieURL + "\(id)" + "/credits?api_key=" + APIKey.TMDB + Language.korean
+        var rtn: [Cast] = []
+        AF.request(creditsUrl, method: .get).validate(statusCode: 200...300).responseData(queue:.global()) { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                //                print(json)
+                for person in json["cast"].arrayValue {
+                    if person["known_for_department"].stringValue == "Acting" {
+                        rtn.append(Cast(name: person["name"].stringValue, profilePath: person["profile_path"].stringValue, character: person["character"].stringValue, id: person["id"].intValue))
+                    }
+                }
+            case .failure(let error):
+                print(error)
+            }
+            self.semaphore.signal()
+        }
+        self.semaphore.wait()
+//        print("@@@@@@@@@@", rtn)
+        return rtn
+    }
+    
+    func getMediaData(startPage:Int, completionHandler: @escaping CompletionHandler) {
+        var list: [Movie] = []
+        var totalPages: Int = 0
+        let trendUrl = EndPoint.trendURL + APIKey.TMDB + "&page=\(startPage)"
+        AF.request(trendUrl, method: .get).validate(statusCode: 200...300).responseData(queue: .global()) { [self] response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                totalPages = json["total_pages"].intValue
+                for movie in json["results"].arrayValue {
+                    semaphore.wait()
+                    let castList: [Cast] = getCast(id: movie["id"].intValue) // 이 함수 호출이 완료되길 기다려주고 싶은데 방법을 모르겠다 => semaphore로 해결!
+                    semaphore.signal()
+                    list.append(Movie(id: movie["id"].intValue, title: movie["title"].stringValue, release: movie["release_date"].stringValue, genreIds: movie["genre_ids"].arrayObject as? [Int] ?? [0], posterPath: movie["poster_path"].stringValue, Overview: movie["overview"].stringValue, casts: castList))
+                }
+            case .failure(let error):
+                print(error)
+            }
+            completionHandler(totalPages,list)
+        }
+    }
+}
--- TMDB/ViewController.swift
+++ TMDB/ViewController.swift
@@ -15,74 +15,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var trendCollectionView: UICollectionView!
     
     var list: [Movie] = []
+    let genres: [Int:String] = TrendMediaAPIManager.shared.getGenresData()
     var startPage = 1
-    var totalCount = 0
+    var totalPages = 0
 
-    let semaphore = DispatchSemaphore(value: 1)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.searchController = searchController
-        
-        getMediaData()
+        setList()
+        print(genres)
         // Do any additional setup after loading the view.
     }
     
-    func setGenres() {
-        let url = EndPoint.genreURL + APIKey.TMDB + Language.korean
-        AF.request(url, method: .get).validate(statusCode: 200...300).responseData { response in
-            switch response.result {
-            case .success(let value):
-                let json = JSON(value)
-                print(json)
-            case .failure(let error):
-                print(error)
+    func setList () {
+        TrendMediaAPIManager.shared.getMediaData(startPage: startPage) { totalPages, list in
+            self.totalPages = totalPages
+            self.list.append(contentsOf: list)
+//            print(self.list)
+//            print("Total  : ", self.totalPages)
+            DispatchQueue.main.async {
+                self.trendCollectionView.reloadData()
             }
         }
     }
     
-    func getCast(id: Int) -> [Cast] {
-        let creditsUrl = EndPoint.movieURL + "\(id)" + "/credits?api_key=" + APIKey.TMDB + Language.korean
-        var rtn: [Cast] = []
-        AF.request(creditsUrl, method: .get).validate(statusCode: 200...300).responseData(queue:.global()) { response in
-            switch response.result {
-            case .success(let value):
-                let json = JSON(value)
-                //                print(json)
-                for person in json["cast"].arrayValue {
-                    if person["known_for_department"].stringValue == "Acting" {
-                        rtn.append(Cast(name: person["name"].stringValue, profilePath: person["profile_path"].stringValue, character: person["character"].stringValue, id: person["id"].intValue))
-                    }
-                }
-            case .failure(let error):
-                print(error)
-            }
-            self.semaphore.signal()
-        }
-        self.semaphore.wait()
-//        print("@@@@@@@@@@", rtn)
-        return rtn
-    }
-    
-    func getMediaData() {
-        let trendUrl = EndPoint.trendURL + APIKey.TMDB + "&page=\(startPage)"
-        AF.request(trendUrl, method: .get).validate(statusCode: 200...300).responseData { [self] response in
-            switch response.result {
-            case .success(let value):
-                let json = JSON(value)
-                for movie in json["results"].arrayValue {
-                    semaphore.wait()
-                    let castList: [Cast] = getCast(id: movie["id"].intValue) // 이 함수 호출이 완료되길 기다려주고 싶은데 방법을 모르겠다 => semaphore로 해결!
-                    semaphore.signal()
-                    list.append(Movie(id: movie["id"].intValue, title: movie["title"].stringValue, release: movie["release_date"].stringValue, genreIds: movie["genre_ids"].arrayObject as? [Int] ?? [0], posterPath: movie["poster_path"].stringValue, Overview: movie["overview"].stringValue, casts: castList))
-                }
-            case .failure(let error):
-                print(error)
-            }
-            trendCollectionView.reloadData()
-            print(list)
-        }
-    }
 }
 
 extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
@@ -99,7 +57,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
 extension ViewController: UICollectionViewDataSourcePrefetching {
     func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
         for indexPath in indexPaths {
-            if list.count - 1 == indexPath.item && list.count < totalCount {
+            if list.count - 1 == indexPath.item && list.count < totalPages {
                 startPage += 1
             }
         }
